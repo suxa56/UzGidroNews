48f01dea18f9350176e6c8bd566acd62f2acd8a6
suxa <suxa5637@mail.ru>
2023-08-03 21:06:18 +0500

    icon, colors

--- app/src/main/java/uz/uzgidro/ugenews/presentation/MainActivity.kt
+++ app/src/main/java/uz/uzgidro/ugenews/presentation/MainActivity.kt
@@ -3,7 +3,6 @@ package uz.uzgidro.ugenews.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import com.google.android.material.color.DynamicColors
 import uz.uzgidro.ugenews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        DynamicColors.applyToActivityIfAvailable(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
