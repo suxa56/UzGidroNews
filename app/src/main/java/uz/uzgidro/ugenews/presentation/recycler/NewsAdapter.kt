5e9193b567f4f36e40f782a6e35739eaa7537c85
suxa <suxa5637@mail.ru>
2023-08-05 14:52:52 +0500

    news image

--- app/src/main/java/uz/uzgidro/ugenews/presentation/recycler/NewsAdapter.kt
+++ app/src/main/java/uz/uzgidro/ugenews/presentation/recycler/NewsAdapter.kt
@@ -3,6 +3,7 @@ package uz.uzgidro.ugenews.presentation.recycler
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.squareup.picasso.Picasso
 import uz.uzgidro.ugenews.databinding.CardNewsBinding
 import uz.uzgidro.ugenews.domain.NewsModel
 
@@ -20,5 +21,6 @@ class NewsAdapter: ListAdapter<NewsModel, NewsViewHolder>(NewsDiffUtil) {
         val item = getItem(position)
 
         holder.binding.cardTitle.text = item.title
+        Picasso.get().load(item.img).fit().centerCrop().into(holder.binding.cardImage)
     }
 }
