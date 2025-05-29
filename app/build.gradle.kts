232576d98fdf9bc0aa16bbd1486536cc26c10808
suxa56 <suxa5600@gmail.com>
2025-05-29 18:30:41 +0500

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,26 +5,27 @@ plugins {
     id("androidx.navigation.safeargs")
 }
 
+
 android {
     namespace = "uz.uzgidro.ugenews"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "uz.uzgidro.ugenews"
         minSdk = 26
-        targetSdk = 33
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -33,12 +34,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -49,6 +50,6 @@ dependencies {
 //    implementation ("com.github.d-max:spots-dialog:1.1@aar")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ buildscript {
         google()
     }
     dependencies {
-        val nav_version = "2.6.0"
+        val nav_version = "2.9.0"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
     }
 }
 
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.android.application") version "8.9.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.android.library") version "7.4.2" apply false
 }
